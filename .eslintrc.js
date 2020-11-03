994783244295303637342d889bed8e066e844494
JianxiLin <jianxi_today@163.com>
2020-11-03 23:25:24 +0800

    品牌管理

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,10 +16,31 @@ module.exports = {
     'html'
   ],
   // add your custom rules here
-  rules: {
-    // allow async-await
+    rules: {
+        // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // js语句结尾必须使用分号
+    'semi': ['off', 'always'],
+    // 三等号
+    'eqeqeq': 0,
+    // 强制在注释中 // 或 /* 使用一致的空格
+    'spaced-comment': 0,
+    // 关键字后面使用一致的空格
+    'keyword-spacing': 0,
+    // 强制在 function的左括号之前使用一致的空格
+    'space-before-function-paren': 0,
+    // 引号类型
+    "quotes": [0, "single"],
+    // 禁止出现未使用过的变量
+    // 'no-unused-vars': 0,
+    // 要求或禁止末尾逗号
+    'comma-dangle': 0,
+    //空行最多不能超过100行
+    "no-multiple-empty-lines": [0, { "max": 100 }],
+    "indent": ["off", 2],
+
   }
 }
